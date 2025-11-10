6ec8b6b2f9f7d648c433b0c1f45b79b7ffcc9853
riskycahayu <riskycahayu83@gmail.com>
2025-11-10 17:29:50 +0700

    Add files via upload

--- stack.cpp
+++ stack.cpp
@@ -1,62 +1,76 @@
 #include <iostream>
-#include "stack.h"
 using namespace std;
 
-void createStack(Stack &S) {
-    S.top = -1;
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+bool isEmpty(Node *top)
+{
+    return top == nullptr;
 }
 
-void push(Stack &S, int x) {
-    if (S.top < MAX - 1) {
-        S.top++;
-        S.info[S.top] = x;
-    }
+void push(Node *&top, int data)
+{
+    Node *newNode = new Node();
+    newNode-> data = data;
+    newNode-> next = top;
+    top = newNode;
 }
 
-int pop(Stack &S) {
-    if (S.top >= 0) {
-        int temp = S.info[S.top];
-        S.top--;
-        return temp;
+int pop(Node *&top)
+{
+    if (isEmpty(top))
+    {
+        cout << "Stack kosong, tidak bisa pop!" << endl;
+        return 0;
     }
-    return -1;
+
+    int poppedData = top->data;
+    Node *temp = top;
+    top = top->next;
+
+    delete temp;
+    return poppedData;
 }
 
-void printInfo(Stack S) {
-    cout << "[TOP] ";
-    for (int i = S.top; i >= 0; i--) {
-        cout << S.info[i] << " ";
+void show(Node *top)
+{
+    if (isEmpty(top))
+    {
+        cout << "Stack kosong." << endl;
+        return;
     }
-    cout << endl;
+
+    cout << "TOP -> ";
+    Node *temp = top;
+
+    while (temp != nullptr)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+
+    cout << "NULL" << endl;
 }
 
-void balikStack(Stack &S) {
-    Stack temp;
-    createStack(temp);
-    while (S.top != -1) {
-        push(temp, pop(S));
-    }
-    S = temp;
-}
+int main()
+{
+    Node *stack = nullptr;
 
-void pushAscending(Stack &S, int x) {
-    // Menyisipkan elemen secara urut naik
-    Stack temp;
-    createStack(temp);
+    push(stack, 10);
+    push(stack, 20);
+    push(stack, 30);
 
-    while (S.top != -1 && S.info[S.top] > x) {
-        push(temp, pop(S));
-    }
-    push(S, x);
-    while (temp.top != -1) {
-        push(S, pop(temp));
-    }
-}
+    cout << "Menampilkan isi stack:" << endl;
+    show(stack);
 
-void getInputStream(Stack &S) {
-    char ch;
-    while (cin.get(ch)) {
-        if (ch == '\n') break;
-        push(S, ch - '0');
-    }
+    cout << "Pop: " << pop(stack) << endl;
+
+    cout << "Menampilkan sisa stack:" << endl;
+    show(stack);
+
+    return 0;
 }
